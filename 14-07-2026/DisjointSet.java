b90e1e58d0f7797449f99bc84cf2861965f3192d
devprashant <kumarjha183@gmail.com>
2026-07-14 23:19:59 +0530

    dsa day 135

---
+++ 14-07-2026/DisjointSet.java
@@ -0,0 +1,62 @@
+class DisjointSet {
+
+    int[] parent;
+    int[] rank;
+    int[] size;
+
+    public DisjointSet(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+            size[i] = 1;
+        }
+    }
+
+    private int findParent(int node) {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = findParent(parent[node]); // Path Compression
+    }
+
+    public boolean find(int u, int v) {
+        return findParent(u) == findParent(v);
+    }
+
+    public void unionByRank(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if (pu == pv)
+            return;
+
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pv] < rank[pu]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if (pu == pv)
+            return;
+
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+}
